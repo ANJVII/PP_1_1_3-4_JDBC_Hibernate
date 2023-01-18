13dce0b3a76979400ae8c734a4c7c44fcbeeff82
Anjela_MV <m.angel.v@mail.ru>
2023-01-18 16:59:43 +0800

    1

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -4,12 +4,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.SQLOutput;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
